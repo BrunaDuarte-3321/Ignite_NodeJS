e1b6370e36311b4c5e1c086ce859b096ec825789
Bruna Duarte <b.elisabete.duarte@parvi.com.br>
2023-09-30 19:10:17 -0300

    feat: Estrutuando aplicação com os metodos http

--- modulo-1/Fundamentos/src/server.js
+++ modulo-1/Fundamentos/src/server.js
@@ -1,7 +1,37 @@
  import http from 'node:http'
 
+
+ const users = []
  const server = http.createServer((req, res) => {
-  return res.end('Hello World')
+  const {method, url} = req
+
+  if(method === 'GET' && url === '/users'){
+    return res.end('Listagem de usuários')
+  } 
+
+  if(method === 'POST' && url ==='/users'){
+   /*  const {} = req
+    const {id, name, email} = req */
+
+    const user = {
+      id: 1,
+      name: "Bruna",
+      email: "Teste"
+    }
+    if(user){
+      users.push(user)
+      return res
+      .send
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
+    }
+    
+    return res.end('Dados pendentes')
+  }
+
+  //Early return 
+
+  return res.writeHead(404).end('Not Found')
 })
 
 server.listen(3333)
